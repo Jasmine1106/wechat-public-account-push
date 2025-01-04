6d75727602791648c29c6155837b6e84018c3045
TheJ <134487218+Jasmine1106@users.noreply.github.com>
2025-01-04 16:23:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ktsL2CE62P7SGIYyEAijdTnMT6s4_T7eQ4bQ4neg4q4',
+      id: 'oRcfp69jFSWeM1HoXcXuwXckLwoc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '1lxoQk7LzH5GlpBDnIBDFStD30ujcbPldNDFwLX88vQ',
       festivals: [
