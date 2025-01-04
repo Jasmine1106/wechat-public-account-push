4e49e6ed53202e32409524a95c687017464826a8
TheJ <134487218+Jasmine1106@users.noreply.github.com>
2025-01-04 16:11:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oRcfp69jFSWeM1HoXcXuwXckLwoc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'BXO3msn2nU8MoN6UbI20yuKdCW_WQkwJatxC-td37oI',
+      useTemplateId: '1lxoQk7LzH5GlpBDnIBDFStD30ujcbPldNDFwLX88vQ',
       festivals: [
         {
           type: '*生日', name: '我家宝宝', year: '2004', date: '08-05', isShowAge: true,
