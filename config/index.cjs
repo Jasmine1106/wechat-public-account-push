6979dafdb4400d3c8b94a69f804775f4fa26bdd7
TheJ <134487218+Jasmine1106@users.noreply.github.com>
2025-01-04 14:59:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,23 +24,20 @@ const USER_CONFIG = {
       id: 'oRcfp69jFSWeM1HoXcXuwXckLwoc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'a9eTiep8xmEUYFOUWfu3lIGjWZLErysExk-ddTYjXXo',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '09-18',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '我家宝宝', year: '2004', date: '09-18',
         },
-        // {
-        //   type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        // },
+        {
+          type: '恋爱一周年', name: '恋爱一周年', year: '2025', date: '01-19',
+        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2024-01-19' },
-        // 结婚纪念日
-        // { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day_1', date: '2025-01-19' }
       ],
     },
   ],
